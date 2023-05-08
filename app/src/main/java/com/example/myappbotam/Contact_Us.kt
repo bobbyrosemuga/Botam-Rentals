60c54b8b3f6f582e487773d6d13fa4aff32e47e1
bobbyrosemugah <bobbyrose273@gmail.com>
2023-05-08 10:07:23 +0300

    first commit

---
+++ app/src/main/java/com/example/myappbotam/Contact_Us.kt
@@ -0,0 +1,11 @@
+package com.example.myappbotam
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Contact_Us : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contact_us)
+    }
+}
--- app/src/main/java/com/example/myappbotam/DashboardActivity.kt
+++ app/src/main/java/com/example/myappbotam/DashboardActivity.kt
@@ -1,11 +1,48 @@
 package com.example.myappbotam
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
 
 class DashboardActivity : AppCompatActivity() {
+    lateinit var text: TextView
+    lateinit var imagebtnone : ImageButton
+    lateinit var imagebtntwo: ImageButton
+    lateinit var imagebtnthree : ImageButton
+    lateinit var imagebtnfour : ImageButton
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+        text = findViewById(R.id.dash)
+        imagebtnone = findViewById(R.id.viewcar)
+        imagebtntwo = findViewById(R.id.book)
+        imagebtnthree = findViewById(R.id.Contact_Us)
+        imagebtnfour = findViewById(R.id.About_Us)
+        // SetOnClick listeners
+        imagebtnone.setOnClickListener {
+            var tembea = Intent(this,CarShow::class.java)
+            startActivity(tembea)
+        }
+
+
+        imagebtntwo.setOnClickListener {
+            var tembea = Intent(this,Booking::class.java)
+            startActivity(tembea)
+        }
+        imagebtnthree.setOnClickListener {
+            var tembea = Intent(this,DashboardActivity::class.java)
+            startActivity(tembea)
+        }
+        imagebtnfour.setOnClickListener {
+            var tembea = Intent(this,DashboardActivity::class.java)
+            startActivity(tembea)
+        }
     }
 }
