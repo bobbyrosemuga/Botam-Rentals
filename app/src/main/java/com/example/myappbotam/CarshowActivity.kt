cf6a8f542f9a55ae25737a6f01f76dc9c894e64e
bobbyrosemugah <bobbyrose273@gmail.com>
2023-05-12 11:00:41 +0300

    first commit

--- app/src/main/java/com/example/myappbotam/CarshowActivity.kt
+++ app/src/main/java/com/example/myappbotam/CarshowActivity.kt
@@ -7,5 +7,9 @@ class CarshowActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_car_show)
+
+
     }
+
+
 }
